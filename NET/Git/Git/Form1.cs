dc57f5bd2b2c11b406a447a7922f14b185b24429
Luis Lozada <luis.lozada@mvm.com.co>
2020-08-11 15:21:59 -0500

    Proyecto git

---
+++ NET/Git/Git/Form1.cs
@@ -0,0 +1,43 @@
+﻿using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Git
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            using (var repo = new Repository(@"E:\Luis\Trabajo\MVM\RepositorioEPM\BaseDeDatos"))
+            {               
+
+                var branches = repo.Branches;
+                foreach (var b in branches)
+                {
+                    Console.WriteLine(b.FriendlyName);
+                }
+
+                foreach (var commit in repo.Commits)
+                {
+                    Console.WriteLine(
+                        $"{commit.Id.ToString().Substring(0, 7)} " +
+                        $"{commit.Author.When.ToLocalTime()} " +
+                        $"{commit.MessageShort} " +
+                        $"{commit.Author.Name}");
+                }
+
+                var branch = repo.Branches["desarrollo"];
+                Commands.Checkout(repo, "desarrollo");
+            }
+        }
+    }
+}
