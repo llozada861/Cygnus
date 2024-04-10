32a3a8434e566ce8c31126dcb437e718bd759528
llozada <luis.lozada@mvm.com.co>
2024-04-10 07:32:59 -0500

    Se cambia la lib personalizada FirstFloor

---
+++ NET/CygnusAzure/Cygnus2-0/Pages/General/WinGenerica.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using res = Cygnus2_0.Properties.Resources;
+
+namespace Cygnus2_0.Pages.General
+{
+    /// <summary>
+    /// Interaction logic for WinGenerica.xaml
+    /// </summary>
+    public partial class WinGenerica : Window
+    {
+        private string accion = res.No;
+        public WinGenerica(UserControl userControls, string titulo, Int64 width = 300, Int64 height = 200, string textProc = "Procesar", string textCanc = "Cancelar", WindowStyle nuWindowStyle = WindowStyle.ThreeDBorderWindow)
+        {
+            InitializeComponent();
+            this.Title = titulo;
+            gridPrincipal.Children.Add(userControls);
+            this.Width = width;
+            this.Height = height;
+            this.btnProcesar.Content = textProc;
+            this.btnCancelar.Content = textCanc;
+            this.WindowStyle = nuWindowStyle;
+        }
+
+        private void btnProcesar_Click(object sender, RoutedEventArgs e)
+        {
+            this.accion = res.Si;
+            this.Close();
+        }
+
+        private void btnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            this.accion = res.No;
+            this.Close();
+        }
+
+        public string Accion
+        {
+            get { return accion; }
+            set { accion = value; }
+        }
+    }
+}
