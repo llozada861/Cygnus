c9da19916c9e24f1579b50b7de8db44a3a572a60
Llozada <luis.lozada@globalmvm.com.co>
2026-05-11 15:53:03 -0500

    Llozada - ajuste pktbl y analiza pl

---
+++ NET/CygnusAzure/Cygnus2-0/Model/Settings/UpdateInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cygnus2_0.Model.Settings
+{
+    public class UpdateInfo
+    {
+        public string Version { get; set; }
+        public string Url { get; set; }
+        public string Checksum { get; set; }
+    }
+}
